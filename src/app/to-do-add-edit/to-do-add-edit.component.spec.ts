7dd22dbe3b50f5e9bc609cc729827f1fc5349c71
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-05-03 20:45:22 +0200

    fix some testing but a lot WIP

--- src/app/to-do-add-edit/to-do-add-edit.component.spec.ts
+++ src/app/to-do-add-edit/to-do-add-edit.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ToDoAddEditComponent } from './to-do-add-edit.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('ToDoAddEditComponent', () => {
   let component: ToDoAddEditComponent;
   let fixture: ComponentFixture<ToDoAddEditComponent>;
+  let matDialogRef: MatDialogRef<ToDoAddEditComponent>;
+
+  let core: CoreService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ToDoAddEditComponent],
+      imports: [HttpClientModule, MatSnackBarModule],
+      providers: [
+        CoreService,
+        MatSnackBar,
+        { provide: MatDialogRef, useValue: matDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     }).compileComponents();
 
+    core = TestBed.inject(CoreService);
     fixture = TestBed.createComponent(ToDoAddEditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
---
+++ src/app/to-do-add-edit/to.do-add-edit.module.ts
@@ -0,0 +1,13 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatFormFieldModule,
+    // other imports
+  ],
+  declarations: [
+    // component declarations
+  ],
+})
+export class ToDoAddEditModule {}
